e8812703ed5aa32e5637d1e7e634850d49b78e5c
Lui <injabie3@gmail.com>
2015-04-26 20:47:30 -0700

    Modified tokenCollection class.
    - Added comments to both .cpp and .h files.

--- LLSIF Assistant/LLSIF Assistant/tokenCollection.cpp
+++ LLSIF Assistant/LLSIF Assistant/tokenCollection.cpp
@@ -10,31 +10,37 @@ tokenCollection::tokenCollection()
 
 }
 
+//Function to set current points.
 void tokenCollection::setCurrentPoints(int points)
 {
 	currentPoints = points;
 }
 
+//Function to get current points.
 int tokenCollection::getCurrentPoints()
 {
 	return currentPoints;
 }
 
+//Function to set current number of tokens.
 void tokenCollection::setCurrentTokens(int tokens)
 {
 	currentTokens = tokens;
 }
 
+//Function to get current number of tokens.
 int tokenCollection::getCurrentTokens()
 {
 	return currentTokens;
 }
 
+//Function to set the estimated number of points.
 void tokenCollection::setEstimatedPoints(int points)
 {
 	estimatedPoints = points;
 }
 
+//Function to get the estimated number of points.
 int tokenCollection::getEstimatedPoints()
 {
 	return estimatedPoints;
--- LLSIF Assistant/LLSIF Assistant/tokenCollection.h
+++ LLSIF Assistant/LLSIF Assistant/tokenCollection.h
@@ -7,20 +7,23 @@ class tokenCollection
 public:
 	tokenCollection();
 
+	//Function to set/get current points.
 	void setCurrentPoints(int points);
 	int getCurrentPoints();
 	
+	//Function to set/get current number of tokens.
 	void setCurrentTokens(int tokens);
 	int getCurrentTokens();
 	
+	//Function to set/get the estimated number of points.
 	void setEstimatedPoints(int points);
 	int getEstimatedPoints();
 
 	//Don't know what this is, but I'll leave it here for now.
 	//~tokenCollection();
 private:
-	int currentPoints;
-	int currentTokens;
-	int estimatedPoints;
+	int currentPoints;			//The user's current event points
+	int currentTokens;			//The user's current event tokens
+	int estimatedPoints;		//The estimated number of points the user has after calculations.
 };
 
