39f0a2bbc3bd9bce2bb3a19cf1c9aa8b1f9239d8
Lui <injabie3@gmail.com>
2015-05-05 13:46:54 -0700

    GUI
    - Removed command prompt window. Program now starts without it.
    Referenced
    http://stackoverflow.com/questions/224225/create-an-application-without-a-window

--- LLSIF Assistant (2013)/LLSIF Assistant (2013)/uiMain.cpp
+++ LLSIF Assistant (2013)/LLSIF Assistant (2013)/uiMain.cpp
@@ -2,12 +2,22 @@
 #include "tokenCollection.h"
 #include "uiMain.h"
 #include "uiAbout.h"
+#include <Windows.h>
 using namespace LLSIFAssistant2013;
 
-int main()
+int WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPTSTR lpCmdLine, int cmdShow)
 {
+	/* do your stuff here. If you return from this function the program ends */
 	tokenCollection uiMainTokenCollection;
 	uiMain FormMain;
 	Application::Run(%FormMain);
 	return 0;
 }
+
+//int main()
+//{
+//	tokenCollection uiMainTokenCollection;
+//	uiMain FormMain;
+//	Application::Run(%FormMain);
+//	return 0;
+//}
