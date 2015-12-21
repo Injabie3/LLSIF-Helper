d73290b369be91a7b448b1d988f69fa17529b0cd
Lui <injabie3@gmail.com>
2015-12-20 16:25:53 -0800

    LLSIFBase class interface update
    Added another private member points_target, and changed setPoints to
    setCurrentPoints to avoid ambiguity.

--- Codes/LLSIFBase.cpp
+++ Codes/LLSIFBase.cpp
@@ -1,12 +1,26 @@
-
+#include "LLSIFBase.h"
 
 //Default Constructor
 LLSIFBase::LLSIFBase()
 {
+	EXP = 0;
+	rank = 1;
+	rank_up = 0;
+
+}
+
+//Other constructor
+LLSIFBase::LLSIFBase(int EXP, int rank, int currentPoints, int targetPoints)
+{
+	this->EXP = EXP;
+	this->rank = rank;
+	this->points_current = currentPoints;
+	this->points_target = targetPoints;
+}
+void LLSIFBase::setMaxLP()
+{
 	
 }
 
-void LLSIFBase::setMaxLP()
-{
-	//y = 34.4512x - 585.5878
-}
+//Use the following to calculate the EXP from the current rank.
+//y = 34.4512x - 585.5878
--- Codes/LLSIFBase.h
+++ Codes/LLSIFBase.h
@@ -10,9 +10,14 @@ public:
 	LLSIFBase();
 	
 	
-	LLSIFBase(int EXP, int rank, int points);
+	LLSIFBase(int EXP, int rank, int currentPoints, int targetPoints);
+	//Precondition:		EXP is the current EXP of the player, expressed as an int, >0.
+	//					rank is the current player's rank, expressed as an int, >0.
+	//					currentPoints is the current number of points the player has, expressed as an int, >=0.
+	//					targetPoints is the number of points the player is aiming for, >= currentPoints.
+	//Postcondition:	
 
-	void setEXP(int EXP) { this.EXP = EXP; };
+	void setEXP(int EXP);
 	//Precondition:		EXP is the current EXP of the player, expressed as an int, and greater than 0.
 	//Postcondition:	Sets the current EXP of the player if valid input is given, else nothing is set.
 	
@@ -24,9 +29,13 @@ public:
 	//Precondition:		currentLP is the current LP that the player has, expressed as an int.  Requires setRank() or a constructor that initializes the rank to be called beforehand.
 	//Postcondition:	Sets the current LP if valid input is given, else sets LP to the maximum.
 
-	void setPoints(int currentPoints);
+	void setCurrentPoints(int currentPoints);
 	//Precondition:		currentPoints is the total number of points the user has, expressed as an int, and greater than or equal to 0.
 	//Postcondition:	Sets the current points if valid input is given, else nothing is set.
+
+	void setTargetPoints(int targetPoints);
+	//Precondition:		A non-default constructor or setCurrentPointstargetPoints has been previously called, and targetPoints is the total number of points the user has, expressed as an int, and greater than or equal to currentPoints.
+	//Postcondition:	Sets the target points if valid input is given, else nothing is set.
 
 	int getEXP();
 	//Precondition:		A non-default constructor or setEXP() member function has been previously called.
@@ -45,10 +54,18 @@ public:
 	//Postcondition:	Returns the maximum LP the user has as an int.
 
 	int getCurrentPoints();
+	//Precondition:		A non-default constructor or setCurrentPoints() member function has been previously called.
+	//Postcondition:	Returns the current number of points the user has as an int.
+
+	int getTargetPoints();
+	//Precondition:		A non-default constructor or setTargetPoints() member function has been previously called.
+	//Postcondition:	Returns the target points the user is aiming for as an int.
+
 	virtual void calculateEstimatedPoints() = 0;
 	//Virtual function to calculate the estimated points the user has.  To be defined in derived classes.
 
 	virtual int getEstimatedPoints() = 0;
+	//Virtual function to get the estimated points the user has.  To be defined in derived classes.
 	
 
 private:
@@ -58,10 +75,11 @@ private:
 	int LP_max;			//The player's maximum LP
 	int LP_current;		//The player's current LP
 	int points_current;	//The player's total points for the event.
-	int LCS_current;
-	int LCS_used;
+	int points_target;	//The number of points the player is aiming for, if applicable.
+	int LCS_current;	//The number of lovecas (LCS) or gems the user currently has.
+	int LCS_used;		//The number of lovecas (LCS) or gems the user has used in the event, if applicable
 
-	void setMaxLP;
+	void setMaxLP();
 };
 
 
